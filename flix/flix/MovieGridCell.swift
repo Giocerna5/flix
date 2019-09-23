3ef35c6e822bab8de2127c3ade700d200a32cca3
Giovanny Cerna <gcerna@csumb.edu>
2019-09-22 21:06:09 -0700

    finished

---
+++ flix/flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by user918030 on 9/22/19.
+//  Copyright © 2019 Matthew Lambert. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
